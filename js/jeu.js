ad7e3bb5c47320abf462c14a482d29194ac8c2ae
ldandoy <ldandoy@overconsulting.net>
2023-03-22 12:49:09 +0100

    MAJ

--- js/jeu.js
+++ js/jeu.js
@@ -1,1 +1,36 @@
-console.error('Jeu démarré')
+console.log('Jeu démarré');
+
+let box = document.querySelector('.box');
+console.log(box);
+let click = 0;
+let scroreElement = document.querySelector('#score');
+
+let chrono = 10;
+let chronoElement = document.querySelector('#chrono');
+chronoElement.innerHTML = chrono;
+
+box.addEventListener("click", () => {
+    console.log('click sur la box !');
+    click += 1;
+    scroreElement.innerHTML = click;
+    
+    let top = Math.floor(Math.random() * window.innerHeight);
+    let left = Math.floor(Math.random() * window.innerWidth);
+
+    // box.style.top = top + "px";
+    box.style.top = `${top}px`;
+    // box.style.backgroundColor = "red";
+    box.style.left = `${left}px`;
+});
+
+setInterval(() => {
+    console.log("timer");
+    if (chrono != 0) {
+        chrono--;
+        chronoElement.innerHTML = chrono;
+    }
+
+    /*if (chrono == 0) {
+        clearInterval()
+    }*/
+}, 1000)
